f85226063c38cab8214cf8a85dbf03a2d27c0c7c
DarianaSoliz <131198358+DarianaSoliz@users.noreply.github.com>
2024-05-04 09:30:58 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace HelloEscenarioForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRotar = new System.Windows.Forms.Button();
+            this.buttonTraslacion = new System.Windows.Forms.Button();
+            this.buttonEscalacion = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRotar
+            // 
+            this.buttonRotar.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRotar.Location = new System.Drawing.Point(109, 88);
+            this.buttonRotar.Name = "buttonRotar";
+            this.buttonRotar.Size = new System.Drawing.Size(145, 54);
+            this.buttonRotar.TabIndex = 0;
+            this.buttonRotar.Text = "ROTACIÓN";
+            this.buttonRotar.UseVisualStyleBackColor = true;
+            this.buttonRotar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonTraslacion
+            // 
+            this.buttonTraslacion.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTraslacion.Location = new System.Drawing.Point(109, 161);
+            this.buttonTraslacion.Name = "buttonTraslacion";
+            this.buttonTraslacion.Size = new System.Drawing.Size(145, 54);
+            this.buttonTraslacion.TabIndex = 1;
+            this.buttonTraslacion.Text = "TRASLACIÓN";
+            this.buttonTraslacion.UseVisualStyleBackColor = true;
+            this.buttonTraslacion.Click += new System.EventHandler(this.buttonTraslacion_Click);
+            // 
+            // buttonEscalacion
+            // 
+            this.buttonEscalacion.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEscalacion.Location = new System.Drawing.Point(104, 234);
+            this.buttonEscalacion.Name = "buttonEscalacion";
+            this.buttonEscalacion.Size = new System.Drawing.Size(150, 54);
+            this.buttonEscalacion.TabIndex = 2;
+            this.buttonEscalacion.Text = "ESCALACIÓN";
+            this.buttonEscalacion.UseVisualStyleBackColor = true;
+            this.buttonEscalacion.Click += new System.EventHandler(this.buttonEscalacion_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(182, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(182, 213);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(220, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ESCENARIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OBJETOS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 345);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 345);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(81, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(314, 345);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 22);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(85, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(212, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(344, 388);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Z";
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleName = "";
+            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.buttonRotar);
+            this.panel1.Controls.Add(this.buttonTraslacion);
+            this.panel1.Controls.Add(this.buttonEscalacion);
+            this.panel1.Location = new System.Drawing.Point(41, 478);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(361, 312);
+            this.panel1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gilroy ExtraBold", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(79, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "TRANSFORMACIONES";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(457, 829);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRotar;
+        private System.Windows.Forms.Button buttonTraslacion;
+        private System.Windows.Forms.Button buttonEscalacion;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿
+using HelloEscenarioRotatorio;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloEscenarioForm
+{
+    public partial class Form1 : Form
+    {
+        private Escenario escenario;
+        private  Dictionary<string, Objeto> objeto = new Dictionary<string, Objeto>();
+
+        Game game;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            escenario = new Escenario(new Vector(-100f, -100f, -100f));
+            escenario.Adicionar("CUBO", CargadorJson.Cargar("../../Objetos/cubo.json"));
+            escenario.Adicionar("PIRAMIDE", CargadorJson.Cargar("../../Objetos/piramide.json"));
+
+            Thread hilo = new Thread(Ejecutar);
+            hilo.Start();
+
+
+            comboBox1.Items.Add("ESCENARIO");
+            foreach (var objeto in escenario.ListaDeObjetos.Keys)
+                comboBox1.Items.Add(objeto);
+
+            comboBox1.SelectedIndex = 0;
+            RetornarCaras();
+        }
+
+        private void Ejecutar()
+        {
+            game = new Game(600, 700, "LearnOpenTK");
+            game.escenario = this.escenario;
+            game.Run();
+        }
+
+        private void RetornarCaras()
+        {
+            comboBox2.Items.Clear();
+            if (comboBox1.SelectedItem.ToString() == "ESCENARIO")
+            {
+                comboBox2.Enabled = false;
+                return;
+            }
+            comboBox2.Enabled = true;
+            comboBox2.Items.Add("OBJETO");
+            foreach (var cara in escenario.GetObjeto(comboBox1.SelectedItem.ToString()).GetListaDeCaras().Keys)
+            {
+                comboBox2.Items.Add(cara);
+            }
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text);
+            float y = float.Parse(textBox2.Text);
+            float z = float.Parse(textBox3.Text);
+
+            if (comboBox1.SelectedItem.ToString() == "ESCENARIO")
+            {
+                escenario.Rotacion(x, y, z);
+                return;
+            }
+            if (comboBox2.SelectedItem.ToString() == "OBJETO")
+            {
+                String llave = comboBox1.SelectedItem.ToString();
+                escenario.GetObjeto(llave).Rotacion(x, y, z);
+                return;
+            }
+            String llaveObjeto = comboBox1.SelectedItem.ToString();
+            String cara = comboBox2.SelectedItem.ToString();
+            Objeto objetoAuxiliar = escenario.GetObjeto(llaveObjeto);
+            objetoAuxiliar.GetCara(cara).Rotacion(x, y, z);
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RetornarCaras();
+        }
+
+        private void buttonTraslacion_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text);
+            float y = float.Parse(textBox2.Text);
+            float z = float.Parse(textBox3.Text);
+
+            if (comboBox1.SelectedItem.ToString() == "ESCENARIO")
+            {
+                escenario.Traslacion(x, y, z);
+                return;
+            }
+            if (comboBox2.SelectedItem.ToString() == "OBJETO")
+            {
+                String llave = comboBox1.SelectedItem.ToString();
+                escenario.GetObjeto(llave).Traslacion(x, y, z);
+                return;
+            }
+            String llaveObjeto = comboBox1.SelectedItem.ToString();
+            String cara = comboBox2.SelectedItem.ToString();
+            Objeto objetoAuxiliar = escenario.GetObjeto(llaveObjeto);
+            objetoAuxiliar.GetCara(cara).Traslacion(x, y, z);
+        }
+
+        private void buttonEscalacion_Click(object sender, EventArgs e)
+        {
+            float x = float.Parse(textBox1.Text);
+            float y = float.Parse(textBox2.Text);
+            float z = float.Parse(textBox3.Text);
+
+            if (comboBox1.SelectedItem.ToString() == "ESCENARIO")
+            {
+                escenario.Escalacion(x, y, z);
+                return;
+            }
+            if (comboBox2.SelectedItem.ToString() == "OBJETO")
+            {
+                String llave = comboBox1.SelectedItem.ToString();
+                escenario.GetObjeto(llave).Escalacion(x, y, z);
+                return;
+            }
+            String llaveObjeto = comboBox1.SelectedItem.ToString();
+            String cara = comboBox2.SelectedItem.ToString();
+            Objeto objetoAuxiliar = escenario.GetObjeto(llaveObjeto);
+            objetoAuxiliar.GetCara(cara).Escalacion(x, y, z);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
